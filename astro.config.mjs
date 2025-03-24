05eb5f8b54becf5db08ea311dd37f2243693b87b
GaetanG2 <gaetanamah@gmail.com>
2025-03-24 18:37:15 +0100

    Commit Project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://GaetanG.github.io',
+  base: '/project_portfolio/',
+  trailingSlash: 'always',
   integrations: [tailwind()],
 });
